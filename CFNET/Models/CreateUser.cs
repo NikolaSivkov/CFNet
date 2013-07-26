2772fe40db39cf37f410cba18e18db5f271402ad
Nikola Sivkov <erasrz@gmail.com>
2013-07-26 14:06:18 +0300

    changed clobberUniqueID to int

--- CFNET/Models/CreateUser.cs
+++ CFNET/Models/CreateUser.cs
@@ -10,7 +10,7 @@
         /// <param name="password">The User's password for the new CloudFlare account. CloudFlare will never store this password in clear text.</param>
         /// <param name="uniqueId">Set a unique string identifying the User. This identifier will serve as an alias to the user's CloudFlare account. Typically you would set this value to the unique ID in your system (e.g., the internal customer number or username stored in your own system). This parameter can be used to retrieve a user_key when it is required. The unique_id must be an ASCII string with a maximum length of 100 characters.</param>
         /// <param name="clobberUniqueId">Any operations that can set a unique_id can be set to automatically "clobber" or unset a previously assigned unique_id.</param>
-        public CFUser(string email,string username,string password,string uniqueId,bool clobberUniqueId =false )
+        public CFUser(string email,string username,string password,string uniqueId,int clobberUniqueId =0 )
         {
             cloudflare_email = email;
             cloudflare_username = username;
@@ -26,7 +26,7 @@
 
         public string unique_id { get; set; }
 
-        public bool clobber_unique_id { get; set; }
+        public int clobber_unique_id { get; set; }
 
         /// <summary>
         /// Represents the action of creating a user.
