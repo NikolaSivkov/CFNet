838ba189c67f4c816900222798bd1697966fa2de
Nikola Sivkov <erasrz@gmail.com>
2015-01-01 17:36:19 +0200

    Add Client Api Functions
    1) Get Zone Settings
    2) Toggle Developer Mode
    3) Clear All Cache

--- Client.cs
+++ Client.cs
@@ -1,12 +1,114 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CFNET.Models;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace CFNET
 {
-    class Client
+    public class Client
     {
+        private RestClient _client;
+
+        #region properties
+
+        public static string ApiUri = "https://www.cloudflare.com/api_json.html";
+        public string ClientApiKey { get; set; }
+        public string ClientEmail { get; set; }
+
+        #endregion
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="clientApiKey">This is the API key made available on your Account page.</param>
+        /// <param name="clientEmail">The e-mail address associated with the API key</param>
+        public Client(string clientApiKey, string clientEmail)
+        {
+            ClientApiKey = clientApiKey;
+            ClientEmail = clientEmail;
+            _client = new RestClient(ApiUri);
+        }
+
+        /// <summary>
+        /// This function will purge CloudFlare of any cached files. It may take up to 48 hours for the cache to rebuild and optimum performance to be achieved so this function should be used sparingly.
+        /// </summary>
+        /// <param name="zone"></param>
+        /// <returns></returns>
+        public dynamic ClearAllCache(string zone)
+        {
+            var request = new RestRequest(Method.POST);
+
+            request.AddParameter("v", "1");
+            request.AddParameter("a", "fpurge_ts");
+            request.AddParameter("z", zone);
+
+            return Execute<ExpandoObject>(request);
+        }
+        
+        /// <summary>
+        /// Retrieves all current settings for a given domain.
+        /// </summary>
+        /// <param name="zone"></param>
+        /// <returns></returns>
+        public ClientZoneSettings GetZoneSettings(string zone)
+        {
+            var request = new RestRequest(Method.POST);
+
+            request.AddParameter("z", zone);
+            request.AddParameter("a", "zone_settings");
+            dynamic response = Execute<ExpandoObject>(request);
+
+            if (response.result != "success")
+            {
+                throw new Exception(response.msg);
+            }
+
+            var serializedObj = JsonConvert.SerializeObject(response.response.result.objs[0]);
+            return JsonConvert.DeserializeObject<ClientZoneSettings>(serializedObj);
+        }
+
+        /// <summary>
+        /// This function allows you to toggle Development Mode on or off for a particular domain. When Development Mode is on the cache is bypassed. Development mode remains on for 3 hours or until when it is toggled back off.
+        /// </summary>
+        /// <param name="zone"></param>
+        /// <returns></returns>
+        public bool ToggleDevMode(string zone)
+        {
+            var zoneSettings = GetZoneSettings(zone);
+
+            var request = new RestRequest(Method.POST);
+            request.AddParameter("a", "devmode");
+            request.AddParameter("z", zone);
+            request.AddParameter("v", zoneSettings.DevMode == 0 ? "1" : "0");
+
+            dynamic requestResilt = Execute<ExpandoObject>(request);
+            if (requestResilt.result != "success")
+            {
+                throw new Exception(requestResilt.msg);
+            }
+
+            return true;
+        }
+
+        private T Execute<T>(RestRequest request) where T : new()
+        {
+            // used on every request
+            request.AddParameter("tkn", ClientApiKey);
+            request.AddParameter("email", ClientEmail);
+
+            var response = _client.Execute<T>(request);
+
+            if (response.ErrorException != null)
+            {
+                throw response.ErrorException;
+            }
+
+            return JsonConvert.DeserializeObject<T>(response.Content);
+        }
     }
 }
---
+++ Models/ClientZoneSettings.cs
@@ -0,0 +1,139 @@
+﻿using Newtonsoft.Json;
+using RestSharp.Deserializers;
+using RestSharp.Serializers;
+
+namespace CFNET.Models
+{
+   
+    public class ClientZoneSettings
+    {
+
+        [JsonProperty("userSecuritySetting")]
+        public string UserSecuritySetting { get; set; }
+
+        /// <summary>
+        ///  Development Mode status.
+        /// A zero response indicates off. 
+        /// A non-zero response indicates on. 
+        /// The numerical value of a non-zero response indicates when Development Mode will expire.
+        /// </summary>
+        [JsonProperty("dev_mode")]
+        public long DevMode { get; set; }
+
+        /// <summary>
+        /// IPV6, Values are [0 = off, 3 = Full]
+        /// </summary>
+        [JsonProperty("ipv46")]
+        public int Ipv46 { get; set; }
+
+        /// <summary>
+        /// Always Online status; Values are [0 = off; 1 = on].
+        /// </summary>
+        [JsonProperty("ob")]
+        public int Ob { get; set; }
+
+        /// <summary>
+        /// Caching Level. Values are: [agg = aggressive, iqs = simplified, basic]
+        /// </summary>
+        [JsonProperty("cache_lvl")]
+        public string CacheLvl { get; set; }
+
+        [JsonProperty("outboundLinks")]
+        public string OutboundLinks { get; set; }
+
+        /// <summary>
+        /// Rocket Loader. Values are: [0 = off, a = automatic, m = manual.]
+        /// </summary>
+        [JsonProperty("async")]
+        public string Async { get; set; }
+
+        [JsonProperty("bic")]
+        public string Bic { get; set; }
+
+        /// <summary>
+        /// Challenge TTL, in seconds.
+        /// </summary>
+        [JsonProperty("chl_ttl")]
+        public string ChlTtl { get; set; }
+
+        /// <summary>
+        /// Expire TTL (for CloudFlare-cached items), in seconds
+        /// </summary>
+        [JsonProperty("exp_ttl")]
+        public string ExpTtl { get; set; }
+
+        [JsonProperty("fpurge_ts")]
+        public string FpurgeTs { get; set; }
+
+        [JsonProperty("hotlink")]
+        public string Hotlink { get; set; }
+
+        /// <summary>
+        ///  [PRO/BUSINESS/ENTERPRISE] Mirage: Auto-resize, Polish settings
+        ///0 = Off
+        ///1 = Auto-resize on
+        ///200 = Polish: Basic
+        ///201 = Polish: Basic, Mirage: Auto-resize on
+        ///170 = Polish: Basic + JPEG
+        ///171 = Polish: Basic + JPEG, Mirage: Auto-resize on
+        /// </summary>
+        [JsonProperty("img")]
+        public string Img { get; set; }
+
+        /// <summary>
+        /// [PRO/BUSINESS/ENTERPRISE] Mirage: Lazy Load. Values are [0 = off, 1 = on]
+        /// </summary>
+        [JsonProperty("lazy")]
+        public string Lazy { get; set; }
+
+        /// <summary>
+        /// Minification
+        ///0 = off
+        ///1 = JavaScript only
+        ///2 = CSS only
+        ///3 = JavaScript and CSS
+        ///4 = HTML only
+        ///5 = JavaScript and HTML
+        ///6 = CSS and HTML
+        ///7 = CSS, JavaScript, and HTML
+        /// </summary>
+        [JsonProperty("minify")]
+        public string Minify { get; set; }
+        
+        [JsonProperty("outlink")]
+        public string Outlink { get; set; }
+
+        /// <summary>
+        /// [PRO/BUSINESS/ENTERPRISE] Preloader. Values are [0 = off, 1 = on]
+        /// </summary>
+        [JsonProperty("preload")]
+        public string Preload { get; set; }
+
+        [JsonProperty("s404")]
+        public string S404 { get; set; }
+
+        /// <summary>
+        /// Basic Security Level. Values are [eoff (Essentially Off), low, med, high, help (I'm Under Attack!)]
+        /// </summary>
+        [JsonProperty("sec_lvl")]
+        public string SecLvl { get; set; }
+
+        /// <summary>
+        /// [PRO/BUSINESS/ENTERPRISE] SPDY. Values are [0 = off, 1 = on]
+        /// </summary>
+        [JsonProperty("spdy")]
+        public string Spdy { get; set; }
+
+        /// <summary>
+        /// [PRO/BUSINESS/ENTERPRISE] SSL Status. Values are [0 = off, 1 = Flexible, 2 = Full, 3 = Full (Strict)]
+        /// </summary>
+        [JsonProperty("ssl")]
+        public string Ssl { get; set; }
+
+        /// <summary>
+        ///  [PRO/BUSINESS/ENTERPRISE] WAF setting. Values are [high, low, off]
+        /// </summary>
+        [JsonProperty("waf_profile")]
+        public string WafProfile { get; set; }
+    }
+}
