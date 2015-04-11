5cb1a461deda35daef902194b4acd148dcd7bcd3
Nikola Sivkov <erasrz@gmail.com>
2015-04-11 19:03:54 +0300

    Remove package

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CFNet")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A wrapper around Cloudflare's client and host APIs")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CFNet")]
